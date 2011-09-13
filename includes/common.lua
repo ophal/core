d5d0a655557e02cc4e6b8045c41dec302d15b9a7
Fernando Paredes García <fernando@develcuy.com>
2011-09-12 23:54:00 -0500

    In function 'page_set_title': Do not assign title when nil given.

--- includes/common.lua
+++ includes/common.lua
@@ -1,5 +1,7 @@
 function page_set_title(header_title, title)
-  if title == nil then title = header_title end
-  ophal.title = title
-  ophal.header_title = (header_title and header_title .. [[ | ]] or [[]]) .. settings.site_name
+  if header_title then
+    if title == nil then title = header_title end
+    ophal.title = title
+    ophal.header_title = (header_title and header_title .. [[ | ]] or [[]]) .. settings.site_name
+  end
 end

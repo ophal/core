49e4b108671099c2af217793e4bb89190ee32b40
Fernando Paredes García <fernando@develcuy.com>
2011-09-12 23:47:36 -0500

    Add support for Header title and Body title to function page_set_title().

--- includes/common.lua
+++ includes/common.lua
@@ -1,3 +1,5 @@
-function page_set_title(title)
-  ophal.title = (title and title .. [[ | ]] or [[]]) .. settings.site_name
+function page_set_title(header_title, title)
+  if title == nil then title = header_title end
+  ophal.title = title
+  ophal.header_title = (header_title and header_title .. [[ | ]] or [[]]) .. settings.site_name
 end

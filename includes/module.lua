bbd16815be846a32f448decb5c202faa0e879451
Fernando Paredes García <fernando@develcuy.com>
2011-09-25 00:29:19 -0500

    Check if hook is implemented before calling it.

--- includes/module.lua
+++ includes/module.lua
@@ -2,14 +2,17 @@ function module_invoke_all(hook, ...)
   local result, r = {}
 
   for name, m in pairs(ophal.modules) do
-    r = m[hook](...) -- function call
-    if type(r) == [[table]] then
-      for path, v in pairs(r) do
-        v.module = name
-        result[path] = v
+    if m[hook] then
+      -- TODO: Error handling
+      r = m[hook](...) -- call hook implementation
+      if type(r) == [[table]] then
+        for k, v in pairs(r) do
+          v.module = name -- register module name
+          result[k] = v
+        end
+      elseif r then
+        table.insert(result, r)
       end
-    else
-      table.insert(result, r)
     end
   end
 

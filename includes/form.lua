091fe668a00d2677d24990d42181358ec33ddc9e
Fernando Paredes Garcia <fernando@develcuy.com>
2012-12-04 00:35:44 -0500

    Implement theme functions: form(), textfield(), label() and button().

---
+++ includes/form.lua
@@ -0,0 +1,55 @@
+
+--[[
+  Form theme function.
+]]
+function theme.form(variables)
+  if variables == nil then variables = {} end
+
+  local default_options = {
+    ['accept-charset'] = [[UTF-8]],
+    method = [[get]],
+  }
+
+  return ([[<form %s action="%s">%s</form>]]):format(
+    render_attributes(variables.attributes, default_options),
+    variables.action,
+    variables.elements
+  )
+end
+
+--[[
+  Input theme function.
+]]
+function theme.textfield(variables)
+  if variables == nil then variables = {} end
+
+  return ('<input %s %s/>'):format(
+    render_attributes(variables.attributes),
+    variables.value and ('value="%s" '):format(variables.value) or ''
+  )
+end
+
+--[[
+  Input theme function.
+]]
+function theme.label(variables)
+  if variables == nil then variables = {} end
+
+  return ('<label %s />%s:</label>'):format(
+    render_attributes(variables.attributes),
+    variables.title
+  )
+end
+
+--[[
+  Input theme function.
+]]
+function theme.button(variables)
+  if variables == nil then variables = {} end
+
+  return ('<input %s type="button" value="%s" />'):format(
+    render_attributes(variables.attributes),
+    variables.value
+  )
+end
+

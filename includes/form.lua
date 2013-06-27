227a035f675935305e0eee9c0f9ba9d997be634d
Fernando Paredes Garcia <fernando@develcuy.com>
2013-06-27 01:59:15 -0500

    Implement theme functions: select() and select_options().

--- includes/form.lua
+++ includes/form.lua
@@ -1,3 +1,4 @@
+local empty, tinsert, tconcat = seawolf.variable.empty, table.insert, table.concat
 
 --[[
   Form theme function.
@@ -87,4 +88,49 @@ function theme.hidden(variables)
     render_attributes(variables.attributes),
     variables.value and ('value="%s" '):format(variables.value) or ''
   )
+end
+
+--[[
+  Returns HTML for a select element.
+]]
+function theme.select(variables)
+  if variables == nil then variables = {} end
+  if variables.class == nil then variables.class = {} end
+
+  local elements, choices, attributes
+
+  elements = variables.elements
+  variables.elements = nil
+
+  choices = variables.choices
+  variables.choices = nil
+
+  variables.class['form-select'] = true
+  variables.class = render_classes(variables.class)
+
+  attributes = render_attributes(variables)
+  if empty(attributes) then attributes = nil end
+  return ('<select%s%s>%s</select>'):format(
+    attributes ~= nil and ' ' or '',
+    attributes or '',
+    theme{'select_options', elements = elements, choices = choices}
+  )
+end
+
+--[[
+  Converts a select element's options array into HTML.
+]]
+function theme.select_options(variables)
+  if variables == nil then variables = {elements = {}, choices = {}} end
+  if variables.elements == nil then variables.elements = {} end
+  if variables.choices == nil then variables.choices = {} end
+
+  local elements, choices, output = variables.elements, variables.choices, {}
+  local selected
+  for k, v in pairs(elements) do
+    selected = not empty(choices[k]) and ' selected="selected"' or ''
+    tinsert(output, ('<option%s>%s</option>'):format(selected, v))
+  end
+
+  return tconcat(output)
 end

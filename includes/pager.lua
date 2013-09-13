fc26810fafbe59b1e9ae5ccd5f6e314abbbc02ae
Fernando Paredes Garcia <fernando@develcuy.com>
2013-09-12 19:11:15 -0500

    Improve pager with HTML nav tag and rel attribute.

--- includes/pager.lua
+++ includes/pager.lua
@@ -3,21 +3,43 @@ local tconcat = table.concat
 function pager(path, num_pages, current_page)
   if nil == current_page then
     current_page = 1
+  elseif type(current_page) ~= 'number' then
+    current_page = tonumber(current_page)
   end
 
   local pages = {}
 
+  if num_pages <= 1 then
+    return pages
+  end
+  -- Link to previous page
+  if current_page > 1 then
+    pages[#pages + 1] = l('Previous', ('%s?page=%s'):format(url(path), current_page - 1), {
+      external = true,
+      attributes = {rel = 'prev'},
+    })
+  end
+
+  -- Build links to all pages
   for page = 1,num_pages do
-    rawset(pages, page,
-      page ~= current_page and
-        l(page, url(path) .. '?page=' .. page, {external = true}) or
-        page
-    )
+    pages[#pages + 1] = page ~= current_page and
+      l(page, url(path) .. '?page=' .. page, {external = true}) or
+      page
+  end
+
+  -- Link to next page
+  if current_page < num_pages then
+    pages[#pages + 1] = l('Next', ('%s?page=%s'):format(url(path), current_page + 1), {
+      external = true,
+      attributes = {rel = 'next'},
+    })
   end
 
   return pages
 end
 
 function theme.pager(pages)
-  return '<div class="pager">Page: ' .. tconcat(pages, ' ') .. '</div>'
+  if #pages > 0 then
+    return '<nav role="navigation" class="pager">' .. tconcat(pages, ' ') .. '</nav>'
+  end
 end
